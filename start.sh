ead429c502fa4e417b10659f1939173a89a0c043
IgorElion <4bewise@gmail.com>
2025-03-26 22:28:28 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-
-# Define o diretório do projeto
-cd /opt/render/project/src
-
-# Cria o diretório de vídeos se não existir
-mkdir -p videos
-
-# Define as variáveis de ambiente
-export PYTHONUNBUFFERED=1
-export TZ=America/Sao_Paulo
-
-# Inicia o bot
-python bot_telegram.py
+   #!/bin/bash
+   python bot_telegram_2.py
